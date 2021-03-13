6b96e9d169994a0e72cb02791de652dbf16c128a
dakshgarera <77096274+dakshgarera@users.noreply.github.com>
2021-03-13 19:12:18 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,16 @@
+class Ground{
+    constructor(){
+        var options={
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(200,390,400,10,options)
+        World.add(world,this.body)
+
+    }
+    display(){
+        rectMode(CENTER)
+        fill ("red")
+        rect(this.body.position.x,this.body.position.y,400,10)
+
+    }
+}
